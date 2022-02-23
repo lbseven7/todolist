1cfe92b252497139ecd4efa6c68e912e578eafb4
Alexsandro Barbosa <leobarbosapinturas@gmail.com>
2022-02-23 20:37:49 -0300

    Pegando os id's no html e armazenando numa variavel

---
+++ script.js
@@ -0,0 +1,7 @@
+// Pegando os id do html e colocando na variavel
+const inputElement = document.getElementById('texto-tarefa');
+const olElement = document.querySelector('#lista-tarefas');
+const btnCreateTarefa = document.querySelector('#criar-tarefa');
+const btnClear = document.getElementById('apaga-tudo');
+const btnRemoveMark = document.getElementById('remover-finalizados');
+
